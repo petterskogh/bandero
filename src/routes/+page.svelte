fa9f36e3d65519f1146346e2c44d6a0110989816
Petter Skogh <petter.skogh@xlent.se>
2023-07-16 17:42:35 +0200

    ✨ Update startpage to be temporary landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>🎸 Bandero 🎸</h1>
+<p>App is under development</p>
+
+<style lang="scss">
+  :global(body) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+
+    font-family: sans-serif;
+    background-color: #121212;
+    color: #FCFCFC;
+  }
+  
+  h1 {
+    font-size: 3rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
+</style>
